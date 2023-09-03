c85a1b3107198028d000141256d939673d3fd66a
Guilherme Duarte <guilhermealduarte@gmail.com>
2023-09-03 14:12:30 -0300

    Create Project

---
+++ src/main/java/com/guilhermeduarte/api/ApiSpingbootApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermeduarte.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpingbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpingbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilhermeduarte/api/ApiSpingbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermeduarte.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpingbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
